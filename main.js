2f00018483b8207a0aa3842ec297152cba198e5c
JooMin Choi <joominchoi23@gmail.com>
2023-12-27 20:49:13 +0000

    Refactor code

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const cityInput = document.getElementById('city-input');
 const searchButton = document.getElementById('search-button');
 const localTimeElement = document.getElementById('local-time');
@@ -7,17 +8,36 @@ const conditionElement = document.getElementById('condition');
 const feelsLikeElement = document.getElementById('feels-like');
 const temperatureUnitButton = document.getElementById('temperature-unit');
 let isCelsius = true;
+let weatherData; // Store weather data in a variable
+
+const myKey = '6e77dbcd41ad4cf5b2a183208232112';
+const defaultCity = 'London';
 
 // Function to fetch weather data
-async function fetchWeatherData(city, apiKey) {
-  const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+async function fetchWeatherData(city) {
+  const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${myKey}&q=${city}`;
 
   const response = await fetch(apiUrl, { mode: 'cors' });
   if (!response.ok) {
-    throw new Error(
-      `Failed to fetch weather data. Status: ${response.status}`);
+    throw new Error(`Failed to fetch weather data. Status: ${response.status}`);
   }
   return response.json();
+}
+
+// Function to output weather information
+async function fetchWeatherInformation() {
+  try {
+    const city = cityInput.value || defaultCity;
+    weatherData = await fetchWeatherData(city); // Update weatherData
+    if (weatherData) {
+      console.log(weatherData);
+      // eslint-disable-next-line no-use-before-define
+      displayWeatherInformation(weatherData);
+      cityInput.value = '';
+    }
+  } catch (error) {
+    console.error('Error fetching weather data:', error.message);
+  }
 }
 
 function dateFormatter(date) {
@@ -35,61 +55,20 @@ function dateFormatter(date) {
   return dateObject.toLocaleString('en-GB', options);
 }
 
-// Function to display weather information
-function displayWeatherInformation(data) {
-  const formattedDateUK = dateFormatter(data.location.localtime);
-  temperatureUnitButton.addEventListener('click', () => {
-    isCelsius = !isCelsius; // Toggle between Celsius and Fahrenheit
-    handleButtonClick(data, isCelsius);
-  });
-
-  localTimeElement.textContent = `Local Time: ${formattedDateUK}`;
-  locationElement.textContent = `Location: ${data.location.name}, ${data.location.country}`;
-  temperatureElement.textContent = `Temperature: ${data.current.temp_c}°C`;
-  conditionElement.textContent = `Condition: ${data.current.condition.text}`;
-  feelsLikeElement.textContent = `Feels Like: ${data.current.feelslike_c}°C`;
-}
-
-// Function to output weather information
-async function fetchWeatherInformation() {
-  const apiKey = '6e77dbcd41ad4cf5b2a183208232112';
-
-  try {
-    const city = cityInput.value || 'London';
-    const responseData = await fetchWeatherData(city, apiKey);
-
-    if (responseData) {
-      console.log(responseData.location, responseData.current);
-      displayWeatherInformation(responseData);
-      cityInput.value = '';
-    }
-  } catch (error) {
-    console.error('Error fetching weather data:', error.message);
-  }
-}
-
-searchButton.addEventListener('click', fetchWeatherInformation);
-
-fetchWeatherInformation();
-
 // Function to update temperature elements
 function updateTemperatures(data, isCelsius) {
-  const temperatureElement = isCelsius
-    ? data.current.temp_c
-    : data.current.temp_f;
-  const feelsLikeElement = isCelsius
-    ? data.current.feelslike_c
-    : data.current.feelslike_f;
+  const temperatureKey = isCelsius ? 'temp_c' : 'temp_f';
+  const feelsLikeKey = isCelsius ? 'feelslike_c' : 'feelslike_f';
 
   return {
-    temperature: temperatureElement,
-    feelsLike: feelsLikeElement,
+    temperature: data.current[temperatureKey],
+    feelsLike: data.current[feelsLikeKey],
   };
 }
 
 // Function to handle button click
-function handleButtonClick(data, isCelsius) {
-  const temperatures = updateTemperatures(data, isCelsius);
+function handleButtonClick(isCelsius) {
+  const temperatures = updateTemperatures(weatherData, isCelsius);
 
   temperatureElement.textContent = `Temperature: ${temperatures.temperature}°${
     isCelsius ? 'C' : 'F'
@@ -98,3 +77,26 @@ function handleButtonClick(data, isCelsius) {
     isCelsius ? 'C' : 'F'
   }`;
 }
+
+// Function to display weather information
+function displayWeatherInformation(data) {
+  const formattedDateUK = dateFormatter(data.location.localtime);
+
+  localTimeElement.textContent = `Local Time: ${formattedDateUK}`;
+  locationElement.textContent = `Location: ${data.location.name}, ${data.location.country}`;
+  temperatureElement.textContent = `Temperature: ${data.current.temp_c}°C`;
+  conditionElement.textContent = `Condition: ${data.current.condition.text}`;
+  feelsLikeElement.textContent = `Feels Like: ${data.current.feelslike_c}°C`;
+}
+
+searchButton.addEventListener('click', fetchWeatherInformation);
+
+temperatureUnitButton.addEventListener('click', () => {
+  isCelsius = !isCelsius; // Toggle between Celsius and Fahrenheit
+  handleButtonClick(isCelsius);
+});
+
+// Initial fetch when the page loads
+fetchWeatherInformation().then(() => {
+  handleButtonClick(isCelsius); // Call handleButtonClick with the initial value
+});
